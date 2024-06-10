3dfd220d9682fe8e950afbdd2d32f3dfb11d57af
Nguyễn Khắc Hậu <22520410@gm.uit.edu.vn>
2024-06-10 10:00:36 +0700

    init lab

---
+++ Lab6/Server.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Lab6
+{
+    partial class Server
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lvHistory = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(192, 53);
+            this.tbX.Name = "tbX";
+            this.tbX.ReadOnly = true;
+            this.tbX.Size = new System.Drawing.Size(99, 22);
+            this.tbX.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Số đang cần tìm";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Phạm vi số đang cần tìm";
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(192, 107);
+            this.tbA.Name = "tbA";
+            this.tbA.ReadOnly = true;
+            this.tbA.Size = new System.Drawing.Size(99, 22);
+            this.tbA.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(297, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "<";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(317, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "X";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(338, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 16);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "<";
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(358, 107);
+            this.tbB.Name = "tbB";
+            this.tbB.ReadOnly = true;
+            this.tbB.Size = new System.Drawing.Size(99, 22);
+            this.tbB.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(573, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 16);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Số người đang tham gia";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(727, 56);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(99, 22);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(645, 113);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Số lượt chơi";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(727, 110);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(99, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Lịch sử trò chơi";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(371, 652);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(125, 41);
+            this.btnStart.TabIndex = 14;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lvHistory
+            // 
+            this.lvHistory.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lvHistory.GridLines = true;
+            this.lvHistory.HideSelection = false;
+            this.lvHistory.Location = new System.Drawing.Point(37, 185);
+            this.lvHistory.Name = "lvHistory";
+            this.lvHistory.Size = new System.Drawing.Size(789, 450);
+            this.lvHistory.TabIndex = 15;
+            this.lvHistory.UseCompatibleStateImageBehavior = false;
+            this.lvHistory.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "";
+            this.columnHeader1.Width = 739;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 716);
+            this.Controls.Add(this.lvHistory);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbX);
+            this.Name = "Server";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListView lvHistory;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
---
+++ Lab6/Server.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Server : Form
+    {
+        
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void SafeCallDelegate(string text);
+        private List<TcpClient> clientsList = new List<TcpClient>();
+        TcpListener listener;
+        int cnt = 1;
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            await Task.Run(() => startListen());
+        }
+
+        void startListen()
+        {
+            try
+            {
+
+                listener = new TcpListener(IPAddress.Any, 8083);
+                while (true)
+                {
+                    listener.Start();
+                    Thread serverThread = new Thread(new ThreadStart(safeServerThread));
+                    serverThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        void safeServerThread()
+        {
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                InforMessages("Client " + cnt.ToString() + " mới đã kết nối vào\n");
+
+                Thread clientThread = new Thread(() => startSafeThread(client));
+                clientThread.Start();
+                cnt++;
+            }
+        }
+
+        void startSafeThread(TcpClient client)
+        {
+            try
+            {
+                clientsList.Add(client);
+                int index = cnt - 1;
+                StreamReader reader = new StreamReader(client.GetStream());
+                while (true)
+                {
+                    string message = reader.ReadLine();
+                    if (message != null)
+                    {
+                        InforMessages(message);
+                        BroadCast(message, client);
+                    }
+                    else
+                    {
+                        InforMessages("Client " + index.ToString() + " đã thoát");
+                        break;
+                    }
+
+                }
+
+                clientsList.Remove(client);
+                reader.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BroadCast(string mess, TcpClient excludedClient)
+        {
+            foreach (TcpClient t in clientsList)
+            {
+                if (t != excludedClient)
+                {
+                    try
+                    {
+                        StreamWriter writer = new StreamWriter(t.GetStream());
+                        writer.WriteLine(mess);
+                        writer.Flush();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
+        }
+
+        private void InforMessages(string mess)
+        {
+            if (lvHistory.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(InforMessages);
+                lvHistory.Invoke(d, new object[] { mess });
+            }
+            else
+            {
+                lvHistory.Items.Add(new ListViewItem(mess));
+            }
+        }
+    }
+}
+
